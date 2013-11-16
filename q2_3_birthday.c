defa7d051bc6f1efed9616e06c6c83800b10bed6
chainsawriot <chainsawtiney@gmail.com>
2013-11-16 22:57:46 +0800

    update

--- q2_3_birthday.c
+++ q2_3_birthday.c
@@ -6,9 +6,14 @@ int main() {
   double matches_me;
   int ct;
   printf("People\t Matches \t Any match\n");
-  for (ct = 2; ct <= 40; ct ++) {
-    matches_me = 1 - pow(364/365., ct - 1);
-    no_match *= (1-(ct-1)/365.);
+  for (ct = 1; ct <= 40; ct ++) {
+    if (ct != 1) {
+      matches_me = 1 - pow(364/365., ct - 1);
+      no_match *= (1-(ct-1)/365.);
+    } else {
+      matches_me = 0;
+      no_match = 1;
+    }
     printf("%i\t %.3f\t\t %.3f\n", ct, matches_me, (1-no_match));
   }
   return 0;
